bbb1c2374e38138b57fe453cac028c48297571dc
PhantomNeon <chho1365@gmail.com>
2022-02-23 02:00:28 +0900

    2022-02-23

---
+++ Phantom/Practice/memo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice
+{
+    class memo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
