fa86b8df49c8296b8735b6cd46b910725e2867ea
Phantom <chho1365@gmail.com>
2022-05-24 18:09:37 +0900

    2022-05-24 phantom [ dll test ] * dll practice in unity

---
+++ Phantom/AwesomepiaLibrary/Dll/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Phantom.Awesomepia
+{
+    public class DllTest
+    {
+        public void Test()
+        {
+            Debug.Log("Awesomepia attack");
+        }
+    }
+}
